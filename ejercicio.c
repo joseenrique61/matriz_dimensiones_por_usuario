b60b761c4f5f33f3fa0dcbda65ff25f026109dbe
joseenrique61 <joseenrique61@hotmail.com>
2023-05-17 15:03:16 -0500

    Se implementa la función obtenerDimensiones

--- ejercicio.c
+++ ejercicio.c
@@ -10,5 +10,27 @@ int n = 0;
 
 int main() 
 {
-    
+    int **matriz = crearMatriz();
+    imprimirMatriz(matriz);
+    free(matriz);
+}
+
+void obtenerDimensiones()
+{
+    printf("Ingrese la cantidad de filas: ");
+    scanf("%i", &m);
+    printf("Ingrese la cantidad de columnas: ");
+    scanf("%i", &n);
+}
+
+int **crearMatriz()
+{
+    obtenerDimensiones();
+
+    int **matriz = calloc(m, sizeof(int *));
+    for (int i = 0; i < m; i++)
+    {
+        matriz[i] = calloc(n, sizeof(int));
+    }
+    return matriz;
 }
