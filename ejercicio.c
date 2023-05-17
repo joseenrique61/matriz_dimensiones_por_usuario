2461befae3fe4cd298a740cf080c2b5dae58b654
joseenrique61 <joseenrique61@hotmail.com>
2023-05-17 15:15:42 -0500

    Se comenta el código

--- ejercicio.c
+++ ejercicio.c
@@ -1,16 +1,20 @@
+//Se incluyen las librerías stdio.h para ingresar datos y mostrarlos al usuario, y stdlib.h para usar las funciones de memoria dinámica.
 #include <stdio.h>
 #include <stdlib.h>
 
+//Se definen los prototipos
 void obtenerDimensiones();
 int **crearMatriz();
 void imprimirMatriz(int **matriz);
 void cambiarDiagonal(int **matriz);
 
+//Se definen las variables, m para filas y n para columnas
 int m = 0;
 int n = 0;
 
 int main()
 {
+    //Se define la matriz como un puntero a puntero para poder crear un array con dimensiones variables, y se le asigna el valor devuelto por crearMatriz.
     int **matriz = crearMatriz();
 
     printf("Matriz inicializada en 0:\n");
@@ -20,9 +24,11 @@ int main()
     printf("Matriz con la diagonal cambiada: \n");
     imprimirMatriz(matriz);
     
+    //Se libera la memoria usada por matriz.
     free(matriz);
 }
 
+//Función que obtiene las dimensiones de los usuarios.
 void obtenerDimensiones()
 {
     printf("Ingrese la cantidad de filas: ");
@@ -31,6 +37,7 @@ void obtenerDimensiones()
     scanf("%i", &n);
 }
 
+//Se crea la matriz usando memoria dinámica, y se definen sus valores en ceros.
 int **crearMatriz()
 {
     obtenerDimensiones();
@@ -43,6 +50,7 @@ int **crearMatriz()
     return matriz;
 }
 
+//Se cambia la diagonal principal de la matriz por unos.
 void cambiarDiagonal(int **matriz)
 {
     for (int i = 0; i < m; i++)
@@ -57,6 +65,7 @@ void cambiarDiagonal(int **matriz)
     }
 }
 
+//Función que imprime la matriz actual.
 void imprimirMatriz(int **matriz)
 {
     for (int i = 0; i < m; i++)
