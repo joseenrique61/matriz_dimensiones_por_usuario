94911366ca66a73a9de9904df22849b20e792957
joseenrique61 <joseenrique61@hotmail.com>
2023-05-17 15:05:59 -0500

    Se implementan las funciones crearMatriz e imprimirMatriz

--- ejercicio.c
+++ ejercicio.c
@@ -34,3 +34,54 @@ int **crearMatriz()
     }
     return matriz;
 }
+
+void imprimirMatriz(int **matriz)
+{
+    for (int i = 0; i < m; i++)
+    {
+        if (m == 1)
+        {
+            printf("[ ");
+        }
+        else
+        {
+            if (i == 0)
+            {
+                printf("%c ", 218);
+            }
+            else if (i == m - 1)
+            {
+                printf("%c ", 192);
+            }
+            else
+            {
+                printf("| ");
+            }
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            printf("%i ", matriz[i][j]);
+        }
+
+        if (m == 1)
+        {
+            printf("] ");
+        }
+        else
+        {
+            if (i == 0)
+            {
+                printf("%c\n", 191);
+            }
+            else if (i == m - 1)
+            {
+                printf("%c\n", 217);
+            }
+            else
+            {
+                printf("|\n");
+            }
+        }
+    }
+}
